a4382890d6437b2319f83bcd86dd4c4160204c5f
Joseph Donato <37385419+jsdonato@users.noreply.github.com>
2021-09-30 18:55:05 -0400

    Update gradient.h
    Added new routine for novel problem

--- gradient.h
+++ gradient.h
@@ -4,7 +4,7 @@
 
 class Gradient {
 public:
-    virtual arma::mat eval(arma::mat x) const = 0;
+    virtual arma::mat Eval(const arma::mat &x) const = 0;
     
     virtual ~Gradient() = default;
 };
@@ -14,13 +14,53 @@ class LinSysGradient : public Gradient {
 public:
     LinSysGradient(arma::mat A_, arma::mat b_) : A(A_), b(b_) {}
     
-    arma::mat eval(arma::mat x) const override {
+    arma::mat Eval(const arma::mat& x) const override {
         return 2 * A.t() * (A * x - b);
     }
     
 private:
-    arma::mat A;
-    arma::mat b;
+    const arma::mat A;
+    const arma::mat b;
+};
+
+//Derived class used to compute \nabla F(X) where F(X)=|C-AXB|_2^2
+class MatrixGradient : public Gradient {
+public:
+    MatrixGradient(arma::mat A_, arma::mat B_, arma::mat C_, double error_weight_) :
+                   A(A_), B(B_), C(C_), error_weight(error_weight_) {}
+    
+    arma::mat Eval(const arma::mat& X) const override {
+        arma::mat result(X.n_rows, X.n_cols);
+        const arma::mat C_minus_AXB = C - A * X * B;
+        
+        for (uint64_t p = 0; p < X.n_rows; p++) {
+            for (uint64_t q = 0; q < X.n_cols; q++) {
+                
+                double sum = error_weight * (1 - kroneckerDelta(p, q)) * X(p,q);
+                for (uint64_t i = 0; i < C_minus_AXB.n_rows; i++) {
+                    for (uint64_t j = 0; j < C_minus_AXB.n_cols; j++) {
+                        sum += C_minus_AXB(i, j) * (-1 * A(i,p) * B(q, j));
+                    }
+                }
+                result(p,q) = 2 * sum;
+                
+            }
+        }
+        
+        
+        return result;
+    }
+    
+    
+private:
+    const arma::mat A;
+    const arma::mat B;
+    const arma::mat C;
+    double error_weight;
+    
+    uint8_t kroneckerDelta(const size_t& i, const size_t& j) const {
+        return (i == j) ? 1 : 0;
+    }
 };
 
 
